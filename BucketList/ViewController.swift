77e08ef34cd3c3b938a99070a8fc68582c31790e
Gleb Fandeev <fandeev.business@gmail.com>
2023-09-19 18:51:33 +0300

    Initial Commit

---
+++ BucketList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BucketList
+//
+//  Created by Gleb Fandeev on 19.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
