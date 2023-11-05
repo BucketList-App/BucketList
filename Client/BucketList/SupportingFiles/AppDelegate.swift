a614a6726eb4c9c8fa784da37d1fc729d6dbda08
Gleb Fandeev <fandeev.business@gmail.com>
2023-11-05 20:39:15 +0300

    test

--- Client/BucketList/SupportingFiles/AppDelegate.swift
+++ Client/BucketList/SupportingFiles/AppDelegate.swift
@@ -26,8 +26,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         appCoordinator.start()
 
-        window!.rootViewController = router.toPresent()
-        window!.makeKeyAndVisible()
+        window?.rootViewController = router.toPresent()
+        window?.makeKeyAndVisible()
 
         return true
     }
